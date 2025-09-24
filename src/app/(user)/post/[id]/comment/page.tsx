a1518f35101e2f1340f82c2f7a34940b0fb0dfaa
wenza007 <overkung4399@gmail.com>
2025-09-25 00:14:10 +0700

    edit comment again

---
+++ src/app/(user)/post/[id]/comment/page.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import BackButton from "@/components/BackButton";
+
+type Comment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+async function getComments(postId: string): Promise<Comment[]> {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
+  );
+  if (!res.ok) throw new Error("Failed to fetch comments");
+  return res.json();
+}
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function CommentPage({ params }: Props) {
+  const comments = await getComments(params.id);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">
+        💬 Comments for Post ID: {params.id}
+      </h1>
+
+      <BackButton />
+
+      <div className="space-y-6">
+        {comments.map((comment) => (
+          <div
+            key={comment.id}
+            className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition"
+          >
+            <h2 className="text-black font-semibold text-lg">{comment.name}</h2>
+            <p className="text-sm text-gray-500 mb-2">{comment.email}</p>
+            <p className="text-gray-700">{comment.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
